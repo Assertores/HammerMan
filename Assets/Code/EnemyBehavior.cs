419341745ced7df7472c4854e388ec1a55c8f287
Unknown <andreas.edmeier@web.de>
2018-05-21 22:55:54 +0200

    Playermovement und Enemy stuff gecoded

---
+++ Assets/Code/EnemyBehavior.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour {
+
+    Rigidbody2D rb;
+
+    bool DirRight = true;
+
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        if (!rb) {
+            throw new System.Exception("Rigitbody not found. Enemy");
+        }
+        if (DirRight) {
+            rb.velocity = this.transform.right;
+        } else {
+            rb.velocity = -this.transform.right;
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (rb.velocity.x <= 0) {
+            if (DirRight) {
+                DirRight = false;
+                rb.velocity = -this.transform.right;
+            } else {
+                DirRight = true;
+                rb.velocity = this.transform.right;
+            }
+        }
+
+	}
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        switch (col.transform.gameObject.tag) {
+            case "Hammer":
+                Die();
+                break;
+            case "Finish":
+
+            default:
+                break;
+        }
+    }
+
+    public void Die() {
+        //here nice sfx and animation
+        //comunicate to gamemanager your dad
+        Destroy(this);
+    }
+}
