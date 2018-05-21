f41a513e4fcc8cd4d5b5f4d2493475f273468a55
Unknown <andreas.edmeier@web.de>
2018-05-21 18:45:26 +0200

    Initialising

---
+++ Assets/Code/PlayerControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+
+    [SerializeField]
+    float PlayerSpeed = 1.0f;
+
+    float goHorizontal = 0.0f;
+    bool goUp = false;
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+    private void FixedUpdate() {
+        InputManager();
+    }
+
+    private void InputManager() {
+        goHorizontal = Input.GetAxis("Horizontal");
+    }
+
+    private void Movement() {
+        
+    }
+}
