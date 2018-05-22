a125194b7fa0a8b0b451a7a0f2b180242fdb95de
Unknown <andreas.edmeier@web.de>
2018-05-22 11:20:31 +0200

    fix enemy

--- Assets/Code/EnemyBehavior.cs
+++ Assets/Code/EnemyBehavior.cs
@@ -8,10 +8,13 @@ public class EnemyBehavior : MonoBehaviour {
     float EnemySpeed = 10;
     [SerializeField]
     float TurningDistance = 10;
+    public LayerMask layer;
 
     Rigidbody2D rb;
 
     bool DirRight = true;
+    bool falling = false;
+    bool startFalling = false;
 
     void Start () {
         rb = GetComponent<Rigidbody2D>();
@@ -27,11 +30,25 @@ public class EnemyBehavior : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (Physics2D.Raycast(this.transform.position, this.transform.forward, TurningDistance)) {
+        
+        if (rb.velocity.y < 0.0f) {
+            falling = true;
+        } else {
+            falling = false;
+            startFalling = true;
+        }
+        if (falling && startFalling) {
+            startFalling = false;
+            DirRight = (Random.value > 0.5f);
+        }
+        RaycastHit2D hit = Physics2D.Raycast(this.transform.position, this.transform.right * rb.velocity.x, TurningDistance, layer);
+        
+        if (hit.collider != null && hit.collider.tag == "Level") {
+            DirRight = !DirRight;
             transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         }
-        rb.velocity = new Vector2(DirRight ? EnemySpeed : -EnemySpeed, rb.velocity.y);
+
+        rb.velocity = new Vector2(DirRight ? EnemySpeed : -EnemySpeed, rb.velocity.y+0.1f);
 	}
 
     private void OnTriggerEnter2D(Collider2D col) {
