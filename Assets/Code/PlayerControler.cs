07c8704f0ed65907ed4ad0c25b3cfbdf3d0a7b6b
Unknown <andreas.edmeier@web.de>
2018-05-30 14:33:13 +0200

    versucht bounciness zu fixen

--- Assets/Code/PlayerControler.cs
+++ Assets/Code/PlayerControler.cs
@@ -28,7 +28,7 @@ public class PlayerControler : MonoBehaviour {
     public int goDown = 0;
     bool DownKeyIsPressed = false;
     bool PlayerIsInGround = false;
-    float LadderX = 0.0f;
+    Vector2 Ladder;
 
 
     private void Start() {
@@ -120,7 +120,7 @@ public class PlayerControler : MonoBehaviour {
     }
 
     void MoveClimb3() {
-        this.transform.position = new Vector2(LadderX, this.transform.position.y);
+        this.transform.position = new Vector2(Ladder.x, this.transform.position.y);
         rb.velocity = new Vector2(0, PlayerSpeed);
     }
 
@@ -152,14 +152,17 @@ public class PlayerControler : MonoBehaviour {
 
     void OnTriggerStay2D(Collider2D col) {
         if (col.transform.gameObject.tag == StringCollection.LADDER) {
-            LadderX = col.transform.position.x;
+            Ladder = col.transform.position;
             isUpPossible = true;
         }
     }
 
     void OnTriggerExit2D(Collider2D col) {
         if (col.transform.gameObject.tag == StringCollection.LADDER) {
-            rb.velocity = new Vector2(rb.velocity.x, 0);
+            if (goUp) {
+                rb.velocity = new Vector2(rb.velocity.x, 0);
+                this.transform.position = new Vector2(this.transform.position.x, Ladder.y + 4.1f);
+            }
             isUpPossible = false;
         }
     }
