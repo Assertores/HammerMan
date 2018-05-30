e118db6188617ada148337b1d61fd3bb864e4fa8
Unknown <andreas.edmeier@web.de>
2018-05-30 14:44:40 +0200

    Kamera Front CutScene funktion aufsetzten

---
+++ Assets/Code/CameraFronScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFronScene : MonoBehaviour {
+
+    [SerializeField]
+    public CameraPathAnchor[] CameraPath;
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Code/CameraPathAnchor.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraPathAnchor
+{
+    public GameObject Position;
+    public float Stay;
+    public float Duration;
+    public float Sharpness;
+}
