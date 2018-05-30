c8ee4ae4ed9c5dfbed0f3fc916790c182f6c7313
Unknown <andreas.edmeier@web.de>
2018-05-30 16:31:33 +0200

    Camera Movement inplimentiert (nochmal ;))

--- Assets/Code/CameraPathAnchor.cs
+++ Assets/Code/CameraPathAnchor.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using UnityEngine;
 
+[Serializable()]
 public class CameraPathAnchor
 {
     public GameObject Position;
+    public float Zoom;
     public float Stay;
     public float Duration;
-    public float Sharpness;
+    public float Sharpness = 2;
 }
