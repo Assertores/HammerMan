02b688e8d7b937bc5efd7d988ef04d057189e26d
Unknown <andreas.edmeier@web.de>
2018-06-12 13:59:59 +0200

    Snaping to grid

---
+++ Assets/Code/SnapToGrid.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour {
+    public float PPU = 32; // pixels per unit (your tile size)
+
+    private void LateUpdate() {
+        Vector3 position = this.transform.position;
+
+        position.x = Mathf.Round(position.x * PPU) / PPU;
+        position.y = Mathf.Round(position.y * PPU) / PPU;
+
+        this.transform.position = position;
+    }
+}
