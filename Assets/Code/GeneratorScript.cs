a693cea4be965b6817d7fbbff2d210f48cd8712d
Andreas <andreas.edmeier@web.de>
2018-07-09 15:51:26 +0200

    Zeuch von spawner in generator verschoben

---
+++ Assets/Code/GeneratorScript.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GeneratorScript : MonoBehaviour {
+
+    [SerializeField]
+    Image LifeBar;
+    [SerializeField]
+    int MaxLife = 1;
+    int CurrentLife;
+    [SerializeField]
+    GameObject SpawnHitParticle;
+    [SerializeField]
+    AudioClip[] HitSound;
+    [SerializeField]
+    AudioClip DeathSound;
+
+    // Use this for initialization
+    void Start () {
+        if (!LifeBar) {
+            throw new System.Exception("WaveBar not assigned. Spawner");
+        }
+        CurrentLife = MaxLife;
+        LifeBar.fillAmount = 1;
+
+        if (!GameManager.ChangeEnemyCount(100)) {
+            LogSystem.LogOnConsole("Spawner konnte nicht den enemyCount erhöhen");
+        }
+    }
+	
+	// Update is called once per frame
+	void OnDestroy () {
+        GameManager.ChangeEnemyCount(-100);
+    }
+
+    private void OnTriggerEnter2D(Collider2D col) {//macht dass es getroffen werden kann
+        if (col.transform.gameObject.tag == StringCollection.HAMMER) {
+            Hit();
+        }
+    }
+
+    private void Hit(int damage = 1) {//TODO: Überarbeiten
+        GameObject handle;
+        CurrentLife -= damage;
+        LifeBar.fillAmount = CurrentLife / (float)MaxLife;
+        if (CurrentLife <= 0) {
+            LogSystem.LogOnConsole("Spawner is dead");// ----- ----- LOG ----- -----
+            handle = Instantiate(SpawnHitParticle, this.transform.position, this.transform.rotation);
+            if (DeathSound != null)
+                handle.GetComponent<AudioSource>().clip = DeathSound;
+            if (handle.GetComponent<ParticleKiller>() == null) {
+                print("ParticalKillerScript kann nicht gefunden werden.");
+            } else {
+                handle.GetComponent<ParticleKiller>().PlayStart();
+            }
+            GameObject.Destroy(this.transform.gameObject);
+            return;
+        }
+        LogSystem.LogOnConsole("Spawner got hit");// ----- ----- LOG ----- -----
+
+        handle = Instantiate(SpawnHitParticle, this.transform.position, this.transform.rotation);
+        if (HitSound.Length > 0) {//kümmert sich um particel
+            int temp = Random.Range(0, HitSound.Length);
+            handle.GetComponent<AudioSource>().clip = HitSound[temp];
+        }
+        if (handle.GetComponent<ParticleKiller>() == null) {
+            print("ParticalKillerScript kann nicht gefunden werden.");
+        } else {
+            handle.GetComponent<ParticleKiller>().PlayStart();
+        }
+    }
+}
--- Assets/Code/SpanerScript.cs
+++ Assets/Code/SpanerScript.cs
@@ -4,23 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SpanerScript : MonoBehaviour {
-
-    [Header("Spawner")]
-    [SerializeField]
-    AudioClip[] HitSound;
-    [SerializeField]
-    AudioClip DeathSound;
-    [SerializeField]
-    Image LifeBar;
-    [SerializeField]
-    int MaxLife = 1;
-    int CurrentLife;
-    [SerializeField]
+    
     [Header("Creaps")]
+    [SerializeField]
     SpawnCreap[] Creaps;
     List<GameObject> Creap1;
-    [SerializeField]
     [Header("Wave")]
+    [SerializeField]
     float SpawnRate;
     [SerializeField]
     float WaveLength;
@@ -34,22 +24,12 @@ public class SpanerScript : MonoBehaviour {
     [Tooltip("0 = no change, 1 = liniar growth, 2 = growing growth, 3 = stagnating growth")]
     int UseFunction;
 
-    [SerializeField]
-    GameObject SpawnHitParticle;
-
     float NextSpawn;
     int WaveCount = 0;
 
 	void Start () {
-        if (!LifeBar) {
-            throw new System.Exception("WaveBar not assigned. Spawner");
-        }
-        CurrentLife = MaxLife;
+        
         NextSpawn = GameManager.GetTime();
-        LifeBar.fillAmount = 1;
-        if (!GameManager.ChangeEnemyCount(100)) {//TODO: gegen Spawner-Generator system ersetzten
-            LogSystem.LogOnConsole("Spawner konnte nicht den enemyCount erhöhen");
-        }
         Creap1 = new List<GameObject>();
         Creap1.Clear();
         for(int i = 0; i < Creaps.Length; i++) {//fügt creaps in der anzahl der Ratio zu ner liste hinzu aus der ein zufälliges element genommen wird
@@ -57,10 +37,6 @@ public class SpanerScript : MonoBehaviour {
                 Creap1.Add(Creaps[i].Creap);
             }
         }
-    }
-
-    private void OnDestroy() {
-        GameManager.ChangeEnemyCount(-100);
     }
 	
 	void Update () {
@@ -109,43 +85,6 @@ public class SpanerScript : MonoBehaviour {
         }
         if (GapLength > MaximumGapLength) {
             GapLength = MaximumGapLength;
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D col) {//macht dass es getroffen werden kann
-        if (col.transform.gameObject.tag == StringCollection.HAMMER) {
-            Hit();
-        }
-    }
-
-    private void Hit(int damage = 1) {
-        GameObject handle;
-        CurrentLife -= damage;
-        LifeBar.fillAmount = CurrentLife / (float)MaxLife;
-        if (CurrentLife <= 0) {
-            LogSystem.LogOnConsole("Spawner is dead");// ----- ----- LOG ----- -----
-            handle = Instantiate(SpawnHitParticle, this.transform.position, this.transform.rotation);
-            if(DeathSound != null)
-                handle.GetComponent<AudioSource>().clip = DeathSound;
-            if (handle.GetComponent<ParticleKiller>() == null) {
-                print("ParticalKillerScript kann nicht gefunden werden.");
-            } else {
-                handle.GetComponent<ParticleKiller>().PlayStart();
-            }
-            GameObject.Destroy(this.transform.gameObject);
-            return;
-        }
-        LogSystem.LogOnConsole("Spawner got hit");// ----- ----- LOG ----- -----
-
-        handle = Instantiate(SpawnHitParticle, this.transform.position, this.transform.rotation);
-        if(HitSound.Length > 0) {//kümmert sich um particel
-            int temp = Random.Range(0, HitSound.Length);
-            handle.GetComponent<AudioSource>().clip = HitSound[temp];
-        }
-        if (handle.GetComponent<ParticleKiller>() == null) {
-            print("ParticalKillerScript kann nicht gefunden werden.");
-        } else {
-            handle.GetComponent<ParticleKiller>().PlayStart();
         }
     }
 }
